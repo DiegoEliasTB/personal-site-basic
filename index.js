7371bde6170116a77d4b46546d74a969c73210db
DiegoEliasTB <diegoeliastb@gmail.com>
2022-08-04 20:51:49 -0300

    :tada: Primeiro commit.

---
+++ index.js
@@ -0,0 +1,36 @@
+function botaoTypescript() {
+  document.getElementById('titulo-tecnologia').textContent = descricoes[0].titulo;
+  document.getElementById('descricao-tecnologia').textContent = descricoes[0].descricao;
+  document.getElementById('link-tecnologia').setAttribute(descricoes[0].link);
+}
+
+function botaoAngular() {
+  document.getElementById('titulo-tecnologia').textContent = descricoes[1].titulo;
+  document.getElementById('descricao-tecnologia').textContent = descricoes[1].descricao;
+  document.getElementById('link-tecnologia').setAttribute(descricoes[1].link);
+}
+
+function botaoReact() {
+  document.getElementById('titulo-tecnologia').textContent = descricoes[2].titulo;
+  document.getElementById('descricao-tecnologia').textContent = descricoes[2].descricao;
+  document.getElementById('link-tecnologia').setAttribute(descricoes[2].link);
+}
+
+let descricoes = [
+  {
+    titulo: 'Typescript',
+    descricao: 'Um superset de javascript que permite a utilização de tipagem e muitas outras ferramentas tornando a linguagem ainda mais poderosa!',
+    link: 'https://www.typescriptlang.org/'
+  },
+  {
+    titulo: 'Angular',
+    descricao: 'Um poderoso framework para front-ends desenvolvido pelo Google!',
+    link: 'https://angular.io/'
+  },
+  {
+    titulo: 'React',
+    descricao: 'Um poderoso framework para front-ends desenvolvido pela Meta!',
+    link: 'https://reactjs.org/'
+  },
+];
+
